df7f8ba5a0e86e4b74c62bc9faded2bc0217ea07
kudoyuto <yuto.kudo@oocl.com>
2021-08-16 20:02:50 +0800

    [reactjsActivity][Yuto]feat: added CounterGroup Component in Components and App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/Counter'
+import CounterGroup from './Components/CounterGroup'
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <CounterGroup></CounterGroup>
     </div>
   );
 }
